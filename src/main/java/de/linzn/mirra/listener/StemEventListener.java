a4bd381a55654eb1dab711bc3d08f7719b14c5e7
Niklas <niklas.linz@enigmar.de>
2024-08-11 14:36:17 +0200

    small fix

--- src/main/java/de/linzn/mirra/listener/StemEventListener.java
+++ src/main/java/de/linzn/mirra/listener/StemEventListener.java
@@ -13,6 +13,7 @@ package de.linzn.mirra.listener;
 
 
 import de.linzn.mirra.MirraPlugin;
+import de.stem.stemSystem.STEMSystemApp;
 import de.stem.stemSystem.modules.eventModule.handler.StemEventHandler;
 import de.stem.stemSystem.modules.informationModule.InformationBlock;
 import de.stem.stemSystem.modules.informationModule.InformationIntent;
@@ -22,11 +23,27 @@ import it.auties.whatsapp.model.jid.Jid;
 public class StemEventListener {
 
     @StemEventHandler()
-    public void inInformationEvent(InformationEvent informationEvent) {
+    public void onInformationEventWhatsapp(InformationEvent informationEvent) {
         InformationBlock informationBlock = informationEvent.getInformationBlock();
         if (informationBlock.hasIntent(InformationIntent.NOTIFY_USER)) {
-            MirraPlugin.mirraPlugin.getWhatsappManager().getWhatsapp().sendChatMessage(Jid.of(MirraPlugin.mirraPlugin.getWhatsappManager().defaultJID), informationBlock.getLongDescription());
-            MirraPlugin.mirraPlugin.getDiscordManager().getJda().retrieveUserById(MirraPlugin.mirraPlugin.getDiscordManager().defaultUID).complete().openPrivateChannel().complete().sendMessage(informationBlock.getLongDescription()).complete();
+            try {
+                MirraPlugin.mirraPlugin.getWhatsappManager().getWhatsapp().sendChatMessage(Jid.of(MirraPlugin.mirraPlugin.getWhatsappManager().defaultJID), informationBlock.getLongDescription());
+            } catch (Exception e) {
+                STEMSystemApp.LOGGER.ERROR(e);
+            }
+
+        }
+    }
+
+    @StemEventHandler()
+    public void onInformationEventDiscord(InformationEvent informationEvent) {
+        InformationBlock informationBlock = informationEvent.getInformationBlock();
+        if (informationBlock.hasIntent(InformationIntent.NOTIFY_USER)) {
+            try {
+                MirraPlugin.mirraPlugin.getDiscordManager().getJda().retrieveUserById(MirraPlugin.mirraPlugin.getDiscordManager().defaultUID).complete().openPrivateChannel().complete().sendMessage(informationBlock.getLongDescription()).complete();
+            } catch (Exception e) {
+                STEMSystemApp.LOGGER.ERROR(e);
+            }
         }
     }
 }
