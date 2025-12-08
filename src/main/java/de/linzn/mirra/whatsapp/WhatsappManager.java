00a7507d347aa0b63902540b17b25d96587662ed
Niklas <niklas.linz@enigmar.de>
2025-12-08 21:05:16 +0100

    disable cobalt temp

--- src/main/java/de/linzn/mirra/whatsapp/WhatsappManager.java
+++ src/main/java/de/linzn/mirra/whatsapp/WhatsappManager.java
@@ -22,6 +22,7 @@ public class WhatsappManager {
         this.defaultJID = MirraPlugin.mirraPlugin.getDefaultConfig().getString("whatsapp.defaultJID", "xxxx");
         this.sessionUUID = UUID.fromString(MirraPlugin.mirraPlugin.getDefaultConfig().getString("whatsapp.sessionUUID", "dd8d7aca-a6cf-468f-a4bf-51c3b8ae7c8a"));
         MirraPlugin.mirraPlugin.getDefaultConfig().save();
+        /*
         try {
             this.whatsapp = WhatsAppClient.builder().webClient()
                     .loadLastOrCreateConnection()
@@ -30,12 +31,11 @@ public class WhatsappManager {
                     .addListener(new OnDisconnectedListener())
                     .addListener(new OnNewChatMessageListener())
                     .connect(); // join removed???
-            //this.whatsapp.store().setTextPreviewSetting(WhatsappTextPreviewPolicy.DISABLED);
             Thread.sleep(1000);
             STEMSystemApp.getInstance().getScheduler().runRepeatScheduler(MirraPlugin.mirraPlugin, this::registerReconnectHandler, 30, 30, TimeUnit.SECONDS);
         } catch (Exception e) {
             STEMSystemApp.LOGGER.ERROR(e);
-        }
+        }*/
     }
 
     private void registerReconnectHandler() {
